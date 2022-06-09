05ba292c60e71653a156abca2ded256592f86464
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-06-09 15:13:36 +0100

    Update readme.md, country name displayed on browser(In a p tag)

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ console.log("HI");
 function getCountryByName(countryName) {
     fetch("https://restcountries.com/v2/name/" + countryName)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => document.querySelector('p').innerText = data[0].name)
+    
+    // variable representing a section in the html with id 'my-section'
+    let mySection = document.querySelector('#my-section');
+    // varibale representing the creation of a p tag
+    let pTag = document.createElement("p");
+    // Append p tag to section in html with id 'my-section'
+    mySection.append(pTag);
+
+    console.log(mySection);
 }
 
 getCountryByName("Nepal");
+
