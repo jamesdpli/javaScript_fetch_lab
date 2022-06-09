cf0b7f4d2e5674f9b0d50728f88f649fdb24e471
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-06-09 19:41:49 +0100

    Create getAllCountries() method

--- app.js
+++ app.js
@@ -6,28 +6,27 @@ function getCountryByName(countryName) {
     .then(data => document.querySelector('p').innerText = "The country is " + data[0].name 
     + " and the population is " + data[0].population + "!")
 
-        // variable representing a section in the html with id 'my-section'
-        let mySection = document.querySelector('#my-section');
-        // varibale representing the creation of a p tag
-        let pTag = document.createElement("p");
-        // Append p tag to section in html with id 'my-section'
-        mySection.append(pTag);
+    // variable representing a section in the html with id 'my-section'
+    let mySection = document.querySelector('#my-section');
+    // varibale representing the creation of a p tag
+    let pTag = document.createElement("p");
+    // Append p tag to section in html with id 'my-section'
+    mySection.append(pTag);
 }
+// getCountryByName("kon")
 
-// function addToAnElement(data){
-
-// }
-
-getCountryByName("kon")
-
-// function getAllCountries() {
-//     fetch("https://restcountries.com/v2/all")
-//     .then(response => response.json())
-//     .then(function(data) { 
-//         data.forEach(
-//             addToAnElement(data)
-//         );
-//     })
-// }
-
-// getAllCountries();
+function getAllCountries() {
+    fetch("https://restcountries.com/v2/all")
+    .then(response => response.json())
+    .then(function(data) { 
+        data.forEach(
+            function(country){
+                let countryName = country.name;
+                let countryPopulation = country.population;
+                let mySection = document.querySelector("#my-section");
+                let pTag = document.createElement("p");
+                pTag.innerText = `The country is ${countryName} its population is ${countryPopulation}` 
+                mySection.appendChild(pTag);
+            })
+    })
+}
